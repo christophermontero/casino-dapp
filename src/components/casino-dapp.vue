9580eb5c1f6944d6b480c2b8bc73715cf308ffae
christophermontero <chris.to20@hotmail.com>
2020-12-08 17:01:48 -0500

    Create a casino-dapp to call data-metamask

---
+++ src/components/casino-dapp.vue
@@ -0,0 +1,11 @@
+<template>
+ <data-metamask/>
+</template><script>
+import DataMetamask from '@/components/data-metamask'
+export default {
+ name: 'casino-dapp',
+ components: {
+ 'hello-metamask': HelloMetamask
+ }
+}
+</script><style scoped></style>
