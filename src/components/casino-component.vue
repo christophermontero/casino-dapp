5ddb7f2db308a1324801442d91a5336acc2ce6ef
christophermontero <chris.to20@hotmail.com>
2020-12-14 18:03:02 -0500

    Create casino-component

---
+++ src/components/casino-component.vue
@@ -0,0 +1,14 @@
+<template>
+  <data-metamask />
+</template>
+<script>
+import DataMetamask from "@/components/data-metamask";
+export default {
+  name: "casino",
+  mounted() {
+    console.log("dispatching getContractInstance");
+    this.$store.dispatch("getContractInstance");
+  },
+};
+</script>
+<style scoped></style>
